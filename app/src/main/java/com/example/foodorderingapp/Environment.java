cbd69cfdc03c2d0986e9e171b24ee2644c680733
Nguyễn Chí Lâm <37863077+Lama2323@users.noreply.github.com>
2024-10-18 00:06:34 +0700

    Delete app/src/main/java/com/example/foodorderingapp/Environment.java

--- app/src/main/java/com/example/foodorderingapp/Environment.java
+++
@@ -1,6 +0,0 @@
-package com.example.foodorderingapp;
-
-public class Environment {
-    public static final String APPLICATION_ID = "A7F988C3-E6C6-4ED4-B258-3EEACCE3EBF3";
-    public static final String API_KEY = "976C4984-0F9F-4B42-AFC4-463813E330AF";
-}
