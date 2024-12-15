5a61a79d0a9d3a7ad8711887ef42346f231b13c2
Nguyễn Chí Lâm <37863077+Lama2323@users.noreply.github.com>
2024-12-15 19:59:16 +0700

    remove main ui

--- app/src/main/java/com/example/foodorderingapp/MainActivity.java
+++ app/src/main/java/com/example/foodorderingapp/MainActivity.java
@@ -3,11 +3,6 @@ package com.example.foodorderingapp;
 import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
 import com.backendless.Backendless;
 import com.backendless.BackendlessUser;
 
@@ -15,15 +10,8 @@ public class MainActivity extends BaseAuthenticatedActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        Backendless.initApp(getApplicationContext(), Environment.APPLICATION_ID, Environment.API_KEY);
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        Backendless.initApp(getApplicationContext(), Environment.APPLICATION_ID, Environment.API_KEY);
     }
 
     @Override
